5b46bc48efb85ea132e2256e0812df4226017c0e
abgonzalez <6ckgwmh41>
2020-01-22 17:46:23 -0600

    personas

---
+++ Sistema/Sistema.Entidades/Ventas/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Entidades.Ventas
+{
+    class Persona
+    {
+        public int idpersona {get; set;}
+        [Required]
+        public string tipo_persona { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3,ErrorMessage = "La persona debe contener mas de 3 caracteres y un minimo de 100")]
+        public string nombre { get; set; }
+        public string tipo_documento { get; set;}
+        public string num_documento { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+
+    }
+}
