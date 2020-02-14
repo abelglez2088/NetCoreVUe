ab025508347450c9fcbb50252512d13ca94a96ba
abgonzalez <6ckgwmh41>
2020-02-13 18:35:49 -0600

    loginNuevo

--- SistemaVue/sistema/src/store/index.js
+++ SistemaVue/sistema/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import decode from 'jwt-decode'
-import router from  '../store/index'
+import router from  '../router/index'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
